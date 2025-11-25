0ead1c799a0730b08bb266e8f35af7cca27d1c97
Philip Tomlinson <philipatomlinson@gmail.com>
2025-11-25 11:24:26 -0500

    update

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,24 @@
     
     "use strict";
 
+/* =================================
+===  MAILCHIMP                 ====
+=================================== */
+    $('.mailchimp').ajaxChimp({
+        callback: mailchimpCallback,
+        url: "http://webdesign7.us6.list-manage.com/subscribe/post?u=9445a2e155b82208d73433060&amp;id=16dc80e353" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
+    });
+
+    function mailchimpCallback(resp) {
+        if (resp.result === 'success') {
+            $('.subscription-success').html('<span class="icon_check_alt2"></span>' + resp.msg).fadeIn(1000);
+            $('.subscription-error').fadeOut(500);
+        }
+        else if (resp.result === 'error') {
+            $('.subscription-error').html('<span class="icon_close_alt2"></span>' + resp.msg).fadeIn(1000);
+        }
+    }
+
 
 /* =================================
 ===  STICKY NAV                 ====
@@ -69,56 +87,40 @@
         effect: 'fadeScale',
     });
 
+
 /* =================================
-===  Netlify              ====
+===  CONTACT FORM               ====
 =================================== */
+    $("#contact").submit(function (e) {
+        e.preventDefault();
+        var name = $("#cf-name").val();
+        var email = $("#cf-email").val();
+        var subject = $("#cf-subject").val();
+        var message = $("#cf-message").val();
+        var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
 
-
-/* $(document).ready(function() {
-    document.getElementById('contact').addEventListener('submit', function(event) {
-        event.preventDefault();
-    
-        fetch("/", {
-            method: "POST",
-            body: new FormData(event.target)
-        })
-        .then(response => {
-            if (response.ok) {
-                // Show custom success message
-                document.getElementById('custom-success-message').style.display = 'block';
-            } else {
-                // Show custom error message
-                document.getElementById('custom-error-message').style.display = 'block';
-            }
-        })
-        .catch(error => {
-            // Show custom error message
-            document.getElementById('custom-error-message').style.display = 'block';
-        });
-    }, { passive:false });}); */
-
-
-    document.getElementById('contact').addEventListener('submit', function(event) {
-        event.preventDefault();
-    
-        fetch("/", {
-            method: "POST",
-            body: new FormData(event.target)
-        })
-        .then(response => {
-            if (response.ok) {
-                document.getElementById('custom-success-message').classList.add('visible');
-                document.getElementById('custom-error-message').classList.remove('visible');
-            } else {
-                document.getElementById('custom-error-message').classList.add('visible');
-                document.getElementById('custom-success-message').classList.remove('visible');
-            }
-        })
-        .catch(error => {
-            document.getElementById('custom-error-message').classList.add('visible');
-            document.getElementById('custom-success-message').classList.remove('visible');
-        });
+        function isValidEmail(emailAddress) {
+            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+            return pattern.test(emailAddress);
+        };
+        if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+            $.ajax({
+                type: "POST",
+                url: "sendmail.php",
+                data: dataString,
+                success: function () {
+                    $('.success').fadeIn(1000);
+                    $('.error').fadeOut(500);
+                }
+            });
+        }
+        else {
+            $('.error').fadeIn(1000);
+            $('.success').fadeOut(500);
+        }
+        return false;
     });
+
 
 /* =================================
 ===  EXPAND COLLAPSE            ====
